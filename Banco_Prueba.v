0e785f675c600d6820c080650c8e5b20ba5a6eb3
Osiris350 <andres.ariascampos@ucr.ac.cr>
2021-06-25 12:33:09 -0700

    Subiendo memoria

---
+++ Banco_Prueba.v
@@ -0,0 +1,32 @@
+`include  "memoria.v"
+
+module bancoPrueba;
+
+
+
+true_dpram_sclk memoria (
+    //OUTPUTS
+    .q_a    (),
+    .q_b    (),
+    //INPUTS
+    .data_a (),
+    .data_b (),
+    .addr_a (),
+    .addr_b (),
+    .we_a   (),
+    .we_b   (),
+    .clk    ()
+);
+
+probador probador1(
+
+
+
+
+
+
+
+);
+
+
+endmodule
